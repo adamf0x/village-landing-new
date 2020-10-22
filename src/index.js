8fc6985e1610624e5d5e0daae7940a6509f8ecf2
Joel <joel@atomic47.co>
2020-10-22 10:23:33 -0700

    Adding google analytics tracking

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import ReactGa from 'react-ga';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,6 +11,11 @@ import * as serviceWorker from './serviceWorker';
 import './assets/scss/style.scss';
 
 const history = createBrowserHistory();
+
+function initializeReactGA() {
+  ReactGa.initialize('UA-161386717-2');
+  ReactGa.pageview('/homepage');
+}
 
 ReactDOM.render(
   <Router history={history}>
--- src/views/Home.js
+++ src/views/Home.js
@@ -12,6 +12,71 @@ const Home = () => {
     <>
       <Hero className="illustration-section-01" />
       <FeaturesTiles />
+      <div className="about-us">
+        <div className="about-us-tile">
+          <img src="src\assets\images\video-placeholder.jpg" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+        </div>
+        <div className="about-us-tile">
+          <img src=".\..\assets\images\family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+        <div className="about-us-tile">
+          <img src="./../../assets/images/family-hero-cropped.png" className="about-us-image"/>
+          <div className="about-us-text">
+            <div className="about-us-name">John Doe</div>
+            <div className="about-us-title">Spaceman</div>
+          </div>
+          
+        </div>
+      </div>
       <FeaturesSplit invertMobile topDivider imageFill className="illustration-section-02" />
       {/* <Testimonial topDivider /> */}
       <Cta split />
